c540652367c081433d0cfc99287f0c9cb9b239fa
Alpay <alpaykilic@sabanciuniv.edu>
2024-04-07 16:55:00 +0300

    editPost, editComment added

---
+++ src/app/api/comments/editComment/route.ts
@@ -0,0 +1,65 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+// edit a comment
+export async function PUT(req: any, res: any) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({
+                status: 401,
+                message: 'Unauthorized',
+            });
+        }
+        const userId = session?.user?.id;
+        const { commentId, content } = await req.json();
+        if (!userId || !commentId || !content) {
+            return NextResponse.json({
+                status: 400,
+                message: 'All fields are required',
+            });
+        }
+
+        // Check if the comment exists
+        const comment = await prisma.comment.findUnique({
+            where: { id: commentId },
+        });
+
+        if (!comment) {
+            return NextResponse.json({
+                status: 404,
+                message: 'Comment not found',
+            });
+        }
+
+        // Check if the user is the owner of the comment
+        if (comment.userId !== userId) {
+            return NextResponse.json({
+                status: 403,
+                message: 'You are not authorized to edit this comment',
+            });
+        }
+
+        // Update the comment
+        const updatedComment = await prisma.comment.update({
+            where: { id: commentId },
+            data: {
+                content: content,
+            },
+        });
+
+        return NextResponse.json({
+            status: 200,
+            message: 'Comment updated',
+            data: updatedComment,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            message: 'Internal server error. Please try again.',
+        });
+    }
+}
---
+++ src/app/api/posts/editPost/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+// edit a post
+export async function PUT(req: any, res: any) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({
+                status: 401,
+                message: 'Unauthorized',
+            });
+        }
+        const userId = session?.user?.id;
+        const { postId, title, content, attachments } = await req.json();
+        if (!userId || !postId || !title || !content) {
+            return NextResponse.json({
+                status: 400,
+                message: 'All fields are required',
+            });
+        }
+
+        // Check if the post exists
+        const post = await prisma.post.findUnique({
+            where: { id: postId },
+        });
+
+        if (!post) {
+            return NextResponse.json({
+                status: 404,
+                message: 'Post not found',
+            });
+        }
+
+        // Check if the user is the owner of the post
+        if (post.userId !== userId) {
+            return NextResponse.json({
+                status: 403,
+                message: 'You are not authorized to edit this post',
+            });
+        }
+
+        // Update the post
+        const updatedPost = await prisma.post.update({
+            where: { id: postId },
+            data: {
+                title: title,
+                content: content,
+                attachments: attachments.length > 0  ? attachments : post.attachments, // Attachments are optional
+            },
+        });
+
+        return NextResponse.json({
+            status: 200,
+            message: 'Post updated',
+            data: updatedPost,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            message: 'Internal server error. Please try again.',
+        });
+    }
+}
