6459d5ccbc039f9a4348b051084eb0b21aede6fb
Alpay <alpaykilic@sabanciuniv.edu>
2024-04-16 12:31:05 +0300

    getting user comments and liked comments added

---
+++ src/app/api/comments/get/getAllUserComments/route.ts
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../auth/[...nextauth]/route";
+
+// get all comments of a user by user id as parameter
+export async function GET(req: any, res: any) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({
+                status: 401,
+                message: 'Unauthorized',
+            });
+        }
+
+        const userId = req.nextUrl.searchParams.get('userId');
+        const comments = await prisma.comment.findMany({
+            where: {
+                userId: userId,
+            },
+            include: {
+                post: {
+                    select: {
+                        title: true,
+                        content: true,
+                        createdAt: true,
+                        updatedAt: true,
+                    },
+                },
+                user: {
+                    select: {
+                        name: true,
+                        username: true,
+                    },
+                },
+            },
+        });
+
+        // If user has not commented on any post
+        if (!comments || comments.length === 0) {
+            return NextResponse.json({
+                status: 404,
+                message: 'No comments found',
+            });
+        }
+
+        return NextResponse.json({
+            status: 200,
+            message: 'Comments found',
+            data: comments,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            message: 'Internal server error. Please try again.',
+        });
+    }
+}
+
---
+++ src/app/api/comments/get/getAllUserLikedComments/route.ts
@@ -0,0 +1,64 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../auth/[...nextauth]/route";
+
+// get all comments liked by a user by user id as parameter
+export async function GET(req: any, res: any) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({
+                status: 401,
+                message: 'Unauthorized',
+            });
+        }
+
+        const userId = req.nextUrl.searchParams.get('userId');
+        const likes = await prisma.commentLike.findMany({
+            where: {
+                userId: userId,
+            },
+            include: {
+                comment: {
+                    include: {
+                        post: {
+                            select: {
+                                title: true,
+                                content: true,
+                                createdAt: true,
+                                updatedAt: true,
+                            },
+                        },
+                        user: {
+                            select: {
+                                name: true,
+                                username: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+        // If user has not liked any comment
+        if (!likes || likes.length === 0) {
+            return NextResponse.json({
+                status: 404,
+                message: 'No liked comments found',
+            });
+        }
+
+        return NextResponse.json({
+            status: 200,
+            message: 'Liked comments found',
+            data: likes,
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            status: 500,
+            message: 'Internal server error. Please try again.',
+        });
+    }
+}
